08fd4957f45b8462960164559c04ce590bd8f528
unknown <cristinakulczynski@gmail.com>
2023-05-15 00:05:23 -0300

    Excluindo arquivos

--- Courses/WEEK II/Complete Java/JavaBootcamp/M1 Introduction/1. Getting Started/Challenge 1/YourInitials.java
+++ Courses/WEEK II/Complete Java/JavaBootcamp/M1 Introduction/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,11 @@
 public class YourInitials {
     public static void main(String[] args) {
-         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+        System.out.println(" CCC   K   K");
+        System.out.println("C   C  K  K ");
+        System.out.println("C      K K  ");
+        System.out.println("C      KK   ");
+        System.out.println("C      K K  ");
+        System.out.println("C   C  K  K ");
+        System.out.println(" CCC   K   K");
     }
 }
--- Courses/WEEK II/Complete Java/JavaBootcamp/M1 Introduction/2. Variables/Challenge 2/Chatbot.java
+++ Courses/WEEK II/Complete Java/JavaBootcamp/M1 Introduction/2. Variables/Challenge 2/Chatbot.java
@@ -1,40 +1,40 @@
 //import Scanner
 
+import java.util.Scanner;
+
 public class Chatbot {
     public static void main(String[] args) {
         
-        //See Learn the Part for the complete instructions. 
-
+        Scanner s = new Scanner (System.in);
         
-        //set up scanner. 
-
         System.out.println("Hello. What is your name?");
-        //Pick up user's name and store it. 
+        String name = s.nextLine();
 
                            
         //add new a line before asking next question. 
-        System.out.println("Hi <name>! I'm Javabot. Where are you from?");
-        //Pick up user's home and store it. 
+        System.out.println("Hi"+ name +" I'm Javabot. Where are you from?");
+        String local = s.nextLine();
                         
 
         //add new a line before asking next question.
-        System.out.println("I hear it's beautiful at <home>! I'm from a place called Oracle");
+        System.out.println("\nI hear it's beautiful at "+ local +"! I'm from a place called Oracle");
         System.out.println("How old are you?");
+        int old = s.nextInt();
         //Pick up age and store it.
 
 
         //add new a line before asking next question.
-        System.out.println("So you're <age>, cool! I'm 400 years old.");
-        System.out.println("This means I'm <divide 400 by age> times older than you.");
+        System.out.println("\nSo you're " + old + ", cool! I'm 400 years old.");
+        System.out.println("This means I'm " + 400/old + "times older than you.");
         System.out.println("Enough about me. What's your favourite language? (just don't say Python)");
-        //Pick up language and store it. 
+        s.nextLine();
+        String language = s.nextLine(); 
 
 
         //add new a line here.
-        System.out.println("<language>, that's great! Nice chatting with you <name>. I have to log off now. See ya!");
+        System.out.println("\n" + language + ", that's great! Nice chatting with you " + name +". I have to log off now. See ya!");
         
-        //close scanner. 
-
+        s.close();
         
     }
 }
